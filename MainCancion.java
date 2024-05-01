b17383f7036d28c4feeded75af878c2797172443
unknown <rantonioivann@gmail.com>
2024-05-01 08:16:45 -0600

    Codigo de cacncion corregido

---
+++ MainCancion.java
@@ -0,0 +1,9 @@
+public class MainCancion {
+
+    public static void main(String[] args) {
+
+        Cancion miCancion = new Cancion();
+        miCancion.crearCancion();
+        System.out.println(miCancion.toString());
+    }
+}
