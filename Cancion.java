7ede4c33b0a086c12bdbe06228494961dc0d5690
unknown <rantonioivann@gmail.com>
2024-05-08 21:18:07 -0600

    v3

--- Cancion.java
+++ Cancion.java
@@ -1,45 +1,57 @@
-import java.util.Scanner;
+import java.util.Scanner; // Importa la clase Scanner del paquete java.util
 
-public class Cancion {
+public class Cancion { // Declara una clase pública llamada Cancion
 
+    // Declara dos variables de instancia (atributos) de tipo String: titulo y autor
     String titulo;
     String autor;
 
+    // Constructor predeterminado vacío
     public Cancion() {
     }
 
+    // Constructor con parámetros para inicializar titulo y autor
     public Cancion(String titulo, String autor) {
-        this.titulo = titulo;
-        this.autor = autor;
+        this.titulo = titulo; // Asigna el valor del parámetro titulo al atributo titulo de la instancia actual
+        this.autor = autor; // Asigna el valor del parámetro autor al atributo autor de la instancia actual
     }
 
+    // Método getter para obtener el valor del atributo titulo
     public String getTitulo() {
         return titulo;
     }
 
+    // Método getter para obtener el valor del atributo autor
     public String getAutor() {
         return autor;
     }
 
+    // Método setter para establecer el valor del atributo titulo
     public void setTitulo(String titulo) {
-        this.titulo = titulo;
+        this.titulo = titulo; // Establece el valor del atributo titulo con el valor del parámetro titulo
     }
 
+    // Método setter para establecer el valor del atributo autor
     public void setAutor(String autor) {
-        this.autor = autor;
+        this.autor = autor; // Establece el valor del atributo autor con el valor del parámetro autor
     }
 
+    // Método toString para devolver una representación en cadena del objeto Cancion
     @Override
     public String toString() {
         return "Cancion " + "titulo=" + titulo + ", autor=" + autor;
     }
 
+    // Método para crear una canción pidiendo al usuario que ingrese el título y el autor
     public void crearCancion() {
-        Scanner leer = new Scanner(System.in);
+        Scanner leer = new Scanner(System.in); // Crea un objeto Scanner llamado leer para leer la entrada del usuario
 
+        // Solicita al usuario que ingrese el título de la canción
         System.out.println("Ingrese el titulo de la cancion");
-        titulo = leer.nextLine();
+        titulo = leer.nextLine(); // Lee la entrada del usuario y la asigna al atributo titulo
+
+        // Solicita al usuario que ingrese el autor de la canción
         System.out.println("Ingrese el autor");
-        autor = leer.nextLine();
+        autor = leer.nextLine(); // Lee la entrada del usuario y la asigna al atributo autor
     }
 }
--- MainCancion.java
+++ MainCancion.java
@@ -1,9 +1,11 @@
 public class MainCancion {
-
-    public static void main(String[] args) {
-
-        Cancion miCancion = new Cancion();
-        miCancion.crearCancion();
-        System.out.println(miCancion.toString());
+    
+    public static void main(String[] args) { // Método principal, punto de entrada del programa
+        
+        Cancion miCancion = new Cancion(); // Crea un objeto de la clase Cancion llamado miCancion
+        
+        miCancion.crearCancion(); // Llama al método crearCancion() del objeto miCancion para crear una canción
+        
+        System.out.println(miCancion.toString()); // Imprime la representación en cadena de la canción utilizando el método toString() y lo muestra en la consola
     }
 }
